70e5132941579d5e0a3261a976c380139a6ffc20
Kaichengxdd <kaichengluo888@gmail.com>
2025-04-02 13:09:26 -0700

    buy 1 color change

--- src/App.tsx
+++ src/App.tsx
@@ -15,16 +15,16 @@ function App() {
   const [totalAura, setTotalAura] = useState(new Decimal(0));
   const [clicks, setClicks] = useState(0);
   const [taita] = useState(
-    () => new slavetemplate("Taita", 0, 0.2, 10, 1000, 100),
+    () => new slavetemplate("Taita", 0, 0.5, 10, 1000, 100),
   );
   const [aadi] = useState(
-    () => new slavetemplate("Aadi", 0, 0.2, 1000, 10000, 500),
+    () => new slavetemplate("Aadi", 0, 0.5, 1000, 10000, 500),
   );
   const [jerry] = useState(
-    () => new slavetemplate("Jerry", 0, 0.2, 100000, 100000, 1000),
+    () => new slavetemplate("Jerry", 0, 0.5, 100000, 100000, 1000),
   );
   const [ayush] = useState(
-    () => new slavetemplate("Ayush", 0, "0.2", 10000000, 1000000, 3000),
+    () => new slavetemplate("Ayush", 0, "0.5", 10000000, 1000000, 3000),
   ); // only string works here to parse BIG values (small is fine) into Decimal
   const [slaves] = useState([taita, aadi, jerry, ayush]);
   const [dummy, setDummy] = useState(0); // dummy state to force re-render
@@ -129,89 +129,95 @@ function App() {
         </button>
       </div>
       <div className="flex flex-row w-full p-8">
-        <Achievements />
+        {/*<Achievements />*/}
         <div className="flex flex-col gap-3 w-full items-center">
-        <div className="w-3/5 flex justify-end">
-          <button
-            onClick={toggleBuyMode}
-            className="btn text-textprimary !w-25 !text-base !h-12 relative overflow-hidden"
-            style={{ backgroundColor: `${buyQuantity === 1 ? "" : "#c09cfa"}` }}
-          >
-            <span className="mix-blend-difference">
-              {buyQuantity === 1 ? "buy one" : "buy max"}
-            </span>
-          </button>
-        </div>
-            <div className="grid gap-3 p-2 w-3/5">
-          {slaves.map((slave) => (
-            <div
-              key={slave.getName()}
-              className="flex items-center gap-4 border border-border rounded-xl p-4"
+          <div className="w-3/5 flex justify-end">
+            <button
+              onClick={toggleBuyMode}
+              className="btn text-textprimary !w-25 !text-base !h-12 relative overflow-hidden"
+              style={{
+                backgroundColor: `${buyQuantity === 1 ? "" : "#c09cfa"}`,
+              }}
             >
-              <div className="relative group">
-                <img
-                  src={
-                    slave.getName() === "Taita"
-                      ? taitaimage
-                      : slave.getName() === "Aadi"
-                        ? aadiimage
-                        : slave.getName() === "Jerry"
-                          ? jerryimage
-                          : slave.getName() === "Ayush"
-                            ? ayushimage
-                            : ""
-                  }
-                  alt={slave.getName()}
-                  className="w-20 h-25 rounded"
-                />
-                <div className="absolute w-100 bottom-full transform mb-1 invisible group-hover:visible p-1.5 bg-gray-900 rounded-2xl border border-border tooltip">
-                  <p className="text-textsecondary text-lg">
-                    Each {slave.getName()} produces {displayNum(slave.getSpeed())}{" "}
-                    {slave.getName() === "Taita"
-                      ? "aura"
-                      : slave.getName() === "Aadi"
-                        ? "Taita"
-                        : slave.getName() === "Jerry"
-                          ? "Aadi"
-                          : slave.getName() === "Ayush"
-                            ? "Jerry"
-                            : "N/A"}{" "}
-                    per second.
+              <span className="mix-blend-difference">
+                {buyQuantity === 1 ? "buy one" : "buy max"}
+              </span>
+            </button>
+          </div>
+          <div className="grid gap-3 p-2 w-3/5">
+            {slaves.map((slave) => (
+              <div
+                key={slave.getName()}
+                className="flex items-center gap-4 border border-border rounded-xl p-4"
+              >
+                <div className="relative group">
+                  <img
+                    src={
+                      slave.getName() === "Taita"
+                        ? taitaimage
+                        : slave.getName() === "Aadi"
+                          ? aadiimage
+                          : slave.getName() === "Jerry"
+                            ? jerryimage
+                            : slave.getName() === "Ayush"
+                              ? ayushimage
+                              : ""
+                    }
+                    alt={slave.getName()}
+                    className="w-20 h-25 rounded"
+                  />
+                  <div className="absolute w-100 bottom-full transform mb-1 invisible group-hover:visible p-1.5 bg-gray-900 rounded-2xl border border-border tooltip">
+                    <p className="text-textsecondary text-lg">
+                      Each {slave.getName()} produces{" "}
+                      {displayNum(slave.getSpeed())}{" "}
+                      {slave.getName() === "Taita"
+                        ? "aura"
+                        : slave.getName() === "Aadi"
+                          ? "Taita"
+                          : slave.getName() === "Jerry"
+                            ? "Aadi"
+                            : slave.getName() === "Ayush"
+                              ? "Jerry"
+                              : "N/A"}{" "}
+                      per second.
+                    </p>
+                  </div>
+                </div>
+                <div className="flex flex-col flex-grow text-textprimary">
+                  <p className="text-4xl">
+                    {slave.getName()}: {displayNum(slave.getAmount())}
+                  </p>
+                  <p className="text-3xl">
+                    {slave.getName()} price: {displayNum(slave.getPrice())}
                   </p>
                 </div>
+                <button
+                  onClick={() =>
+                    buyQuantity === 1 ? handleBuy(slave) : handleMultiBuy(slave)
+                  }
+                  className="btn relative overflow-hidden text-textsecondary"
+                >
+                  <div
+                    className="absolute left-0 top-0 bg-btnlightbg h-full transition-all duration-300 ease-in-out animate-flash"
+                    style={{
+                      width:
+                        buyQuantity === 1
+                          ? `${aura.lt(slave.getPrice()) ? 0 : slave.getNumBought().mod(10).multiply(10).toNumber() + 10}%`
+                          : `${(slave.getNumBought().mod(10).toNumber() + (aura.divide(slave.getPrice()).gt(new Decimal(10)) ? new Decimal(10).subtract(slave.getNumBought().mod(10)).toNumber() : aura.divide(slave.getPrice()).floor().toNumber())) * 10}%`, // also fine here
+                    }}
+                  ></div>
+                  <div
+                    className="absolute left-0 top-0 bg-btnbg h-full transition-all duration-300 ease-in-out"
+                    style={{
+                      width: `${slave.getNumBought().mod(10).multiply(10).toNumber()}%`, // tonumber is safe as it never gets too big
+                    }}
+                  ></div>
+                  <span className="relative z-10 flex items-center justify-center w-full h-full mix-blend-difference">
+                    Increase {slave.getName()}
+                  </span>
+                </button>
               </div>
-              <div className="flex flex-col flex-grow text-textprimary">
-                <p className="text-4xl">
-                  {slave.getName()}: {displayNum(slave.getAmount())}
-                </p>
-                <p className="text-3xl">
-                  {slave.getName()} price: {displayNum(slave.getPrice())}
-                </p>
-              </div>
-              <button
-                onClick={() =>
-                  buyQuantity === 1 ? handleBuy(slave) : handleMultiBuy(slave)
-                }
-                className="btn relative overflow-hidden text-textsecondary"
-              >
-                <div
-                  className="absolute left-0 top-0 bg-btnlightbg h-full transition-all duration-300 ease-in-out animate-flash"
-                  style={{
-                    width: `${(slave.getNumBought().mod(10).toNumber() + (aura.divide(slave.getPrice()).gt(new Decimal(10)) ? new Decimal(10).subtract(slave.getNumBought().mod(10)).toNumber() : aura.divide(slave.getPrice()).floor().toNumber())) * 10}%`, // also fine here
-                  }}
-                ></div>
-                <div
-                  className="absolute left-0 top-0 bg-btnbg h-full transition-all duration-300 ease-in-out"
-                  style={{
-                    width: `${slave.getNumBought().mod(10).multiply(10).toNumber()}%`, // tonumber is safe as it never gets too big
-                  }}
-                ></div>
-                <span className="relative z-10 flex items-center justify-center w-full h-full mix-blend-difference">
-                  Increase {slave.getName()}
-                </span>
-              </button>
-            </div>
-          ))}
+            ))}
           </div>
         </div>
       </div>
